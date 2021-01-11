30a00647b9842289f86e4891ac8dd8076398d336
heewoo0419 <heewoo0419@gmail.com>
2021-01-11 15:43:00 +0900

    ADD : 기본 UI 추가

--- OmokServerDlg.h
+++ OmokServerDlg.h
@@ -11,7 +11,8 @@ class COmokServerDlg : public CDialogEx
 // 생성입니다.
 public:
 	COmokServerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
-
+	CSocServer m_socServer;		// 서버용 소켓
+	CSocCom* m_socCom;			// 통신용 소켓
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_OMOKSERVER_DIALOG };
@@ -30,5 +31,27 @@ protected:
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg LPARAM OnAccept(UINT wParam, LPARAM lParam);
+	afx_msg LPARAM OnReceive(UINT wParam, LPARAM lParam);
 	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_list;
+	CString m_strConnect;
+	CString m_strMe;
+	CString m_strStatus;
+
+
+	int m_iOrder;
+
+	BOOL m_bConnect;	// 다른쪽과 접속했을때 TRUE
+	//BOOL m_bStartSvr;	// 서버가 준비 끝나면 TRUE
+	BOOL m_bStartCnt;	// 클라이언트가 준비 끝나면 TRUE
+	BOOL m_bStart;		// 1~25를 다 채우면 TRUE
+	BOOL m_bMe;			// 내가 선택할 차예이면 TRUE
+	BOOL m_bCntEnd;		// 클라이언트가 끝났는지 검사
+	BOOL m_bSvrEnd;		// 서버가 끝났는지 검사
+
+
+
+	void SendGame(int iType, CString strTmp);
 };
