0421c1b66de7a94d3ed9f79f6a80057c09a4ec43
huijae kim <siggnoral@gmail.com>
2021-01-13 18:24:45 +0900

    add: 무르기 초기커밋

--- Resource.h
+++ Resource.h
@@ -20,6 +20,7 @@
 #define IDC_BUTTON_GIVEUP               1010
 #define IDC_STATIC_BLACKSCORE           1011
 #define IDC_STATIC_WHITESCORE           1012
+#define IDC_BUTTON_UNDO                 1013
 
 // Next default values for new objects
 // 
@@ -27,7 +28,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
